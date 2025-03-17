baa267bbe2d3fab52b5ce0a8bf3db4ba4464eab6
Sandeep Sheriker Mallikarjun <sandeep.sheriker@microchip.com>
2025-03-17 13:43:37 -0700

    MH3-111768: app.c: code cleanup and removed any unused code.
    Signed-off-by: Sandeep Sheriker Mallikarjun <sandeep.sheriker@microchip.com>

--- apps/libcamera_display/firmware/src/app.c
+++ apps/libcamera_display/firmware/src/app.c
@@ -21,51 +21,12 @@
     files.
  *******************************************************************************/
 
-// *****************************************************************************
-// *****************************************************************************
-// Section: Included Files
-// *****************************************************************************
-// *****************************************************************************
-
 #include "app.h"
 
-#define CANVAS_XPOS    0
-#define CANVAS_YPOS    0
-#define CANVAS_WIDTH   800
-#define CANVAS_HEIGHT  480
-
 #define ISC_CANVAS_ID 0
-#define ISC_CANVAS_LAYER 1
-
-// *****************************************************************************
-// *****************************************************************************
-// Section: Global Data Definitions
-// *****************************************************************************
-// *****************************************************************************
-
-// *****************************************************************************
-/* Application Data
-
-  Summary:
-    Holds application data
-
-  Description:
-    This structure holds the application's data.
-
-  Remarks:
-    This structure should be initialized by the APP_Initialize function.
-
-    Application strings and buffers are be defined outside this structure.
-*/
+#define ISC_CANVAS_LAYER 0
 
 APP_DATA appData;
-
-uint8_t canvasfb01[CANVAS_WIDTH * CANVAS_HEIGHT * 2] = { 0 };
-// *****************************************************************************
-// *****************************************************************************
-// Section: Application Callback Functions
-// *****************************************************************************
-// *****************************************************************************
 
 void camera_callback(uintptr_t context)
 {
@@ -79,37 +40,12 @@ void camera_callback(uintptr_t context)
     SYS_MODULE_OBJ object = (SYS_MODULE_OBJ) context;
 
     CAMERA_Get_Frame(object, &addr, &width, &height);
-    //printf("\n\r width = %ld height = %ld \r\n",width, height);
     if (addr != 0)
     { 
         gfxcSetPixelBuffer(ISC_CANVAS_ID, width, height, GFX_COLOR_MODE_RGB_565, (void *)addr);  
         gfxcCanvasUpdate(ISC_CANVAS_ID);
     }
 }
-// *****************************************************************************
-// *****************************************************************************
-// Section: Application Local Functions
-// *****************************************************************************
-// *****************************************************************************
-
-
-/* TODO:  Add any necessary local functions.
-*/
-
-
-// *****************************************************************************
-// *****************************************************************************
-// Section: Application Initialization and State Machine Functions
-// *****************************************************************************
-// *****************************************************************************
-
-/*******************************************************************************
-  Function:
-    void APP_Initialize ( void )
-
-  Remarks:
-    See prototype in app.h.
- */
 
 void APP_Initialize ( void )
 {
@@ -117,22 +53,8 @@ void APP_Initialize ( void )
     appData.state = APP_STATE_INIT;
 }
 
-
-/******************************************************************************
-  Function:
-    void APP_Tasks ( void )
-
-  Remarks:
-    See prototype in app.h.
- */
-
 void APP_Tasks ( void )
 {
-    int i = 0;
-    uint32_t bufferPtr = 0;
-    uint32_t width = CANVAS_WIDTH;
-    uint32_t height = CANVAS_HEIGHT;
-    /* Check the application's current state. */
     switch ( appData.state )
     {
         /* Application's initial state. */
@@ -142,35 +64,9 @@ void APP_Tasks ( void )
             if (appInitialized)
             {    
                 gfxcSetLayer(ISC_CANVAS_ID, ISC_CANVAS_LAYER);
-                gfxcSetWindowSize(0, width, height);
-                gfxcSetWindowPosition(0, 0, 0);    
-                gfxcShowCanvas(0);
-                appData.state = APP_STATE_SERVICE_TASKS;
+                gfxcShowCanvas(ISC_CANVAS_ID);
+                appData.state = APP_STATE_CAMERA_OPEN;
             }
-            break;
-        }
-        case APP_STATE_SERVICE_TASKS:
-        {
-            // START OF CUSTOM CODE
-            static bool once = true;            
-            if(once)
-            {
-                for(i=0; i < sizeof(canvasfb01); i=i+2)
-                {
-                    canvasfb01[i] = 0x73;
-                    canvasfb01[i+1] = 0x93;
-                }
-                gfxcSetPixelBuffer(ISC_CANVAS_ID,
-                               CANVAS_WIDTH,
-                               CANVAS_HEIGHT,
-                               GFX_COLOR_MODE_RGB_565,
-                               (void *)canvasfb01);
-            
-            gfxcShowCanvas(ISC_CANVAS_ID);
-            gfxcCanvasUpdate(ISC_CANVAS_ID);   
-                once = false;
-            }
-            appData.state = APP_STATE_CAMERA_OPEN;
             break;
         }
         case APP_STATE_CAMERA_OPEN:
@@ -200,27 +96,6 @@ void APP_Tasks ( void )
             }
             break;
         }
-        case APP_STATE_CAMERA_GET_FRAME:
-        {            
-            if (sysObj.devCamera != SYS_MODULE_OBJ_INVALID)
-            {
-                bufferPtr = 0;
-                CAMERA_Stop_Capture(sysObj.devCamera);
-                CAMERA_Get_Frame(sysObj.devCamera, &bufferPtr, &width, &height);
-                appData.state = APP_STATE_CANVAS_UPDATE;
-            }
-            break;
-        }        
-        case APP_STATE_CANVAS_UPDATE:
-        {
-            if (bufferPtr != 0)
-            {        
-                gfxcSetPixelBuffer(ISC_CANVAS_ID, width, height, GFX_COLOR_MODE_RGB_565, (void *)bufferPtr);  
-                gfxcCanvasUpdate(ISC_CANVAS_ID);
-            }
-            appData.state = APP_STATE_CAMERA_START;            
-            break;
-        }
         case APP_STATE_IDLE:
         {
             appData.state = APP_STATE_IDLE;            
@@ -234,7 +109,6 @@ void APP_Tasks ( void )
         }
     }
 }
-
 
 /*******************************************************************************
  End of File
